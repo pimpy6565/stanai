4d790a4b7e40d05cdf66ccd46e80fb3f03d46fce
pimpy6565 <155408888+pimpy6565@users.noreply.github.com>
2025-08-13 03:57:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,send_from_directory
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
